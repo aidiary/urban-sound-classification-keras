5f7ac914fe84fa27c397edb63990ef2d20ed8697
aidiary <f2forest@gmail.com>
2017-09-15 11:35:34 +0900

    Update train

--- train.py
+++ train.py
@@ -1,15 +1,26 @@
+import os
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
 from keras.callbacks import CSVLogger, ModelCheckpoint
 
+
+if not os.path.exists('models'):
+    os.mkdir('models')
+
+train_features = np.load('data/train_features.npy')
+train_labels = np.load('data/train_labels.npy')
+test_features = np.load('data/test_features.npy')
+test_labels = np.load('data/test_labels.npy')
+
 # create model
 model = Sequential()
-model.add(Dense(n_hidden_units_1, input_shape=(n_dim, )))
+model.add(Dense(280, input_shape=(128, )))
 model.add(Activation('relu'))
-model.add(Dense(n_hidden_units_2))
+model.add(Dense(300))
 model.add(Activation('relu'))
-model.add(Dense(n_classes))
+model.add(Dense(10))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -17,8 +28,8 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # callbacks
-logger = CSVLogger('train.log')
-weight_file = 'train.{epoch:02d}-{val_loss:.3f}.h5'
+logger = CSVLogger('models/train.log')
+weight_file = 'models/train.{epoch:02d}-{val_loss:.3f}-{val_acc:.3f}.h5'
 checkpoint = ModelCheckpoint(weight_file,
                              monitor='val_loss',
                              verbose=1,
