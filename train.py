0b5b9bc0d911284eb609694d36d3684e69268fb0
aidiary <f2forest@gmail.com>
2017-09-15 15:37:59 +0900

    Add scaler

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
 from keras.callbacks import CSVLogger, ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 if not os.path.exists('models'):
@@ -14,7 +15,19 @@ train_labels = np.load('data/train_labels.npy')
 test_features = np.load('data/test_features.npy')
 test_labels = np.load('data/test_labels.npy')
 
-# create model
+print(np.mean(train_features), np.std(train_features))
+print(np.mean(test_features), np.std(test_features))
+
+# scale train/test data
+#scaler = MinMaxScaler(feature_range=(0, 1))
+scaler = StandardScaler()
+scaler = scaler.fit(train_features)
+train_features = scaler.transform(train_features)
+test_features = scaler.transform(test_features)
+
+print(np.mean(train_features), np.std(train_features))
+print(np.mean(test_features), np.std(test_features))
+
 model = Sequential()
 model.add(Dense(280, input_shape=(128, )))
 model.add(Activation('relu'))
@@ -39,7 +52,7 @@ checkpoint = ModelCheckpoint(weight_file,
 # train
 model.fit(train_features, train_labels,
           batch_size=32,
-          epochs=50,
+          epochs=128,
           verbose=1,
           validation_data=(test_features, test_labels),
           callbacks=[logger, checkpoint])
