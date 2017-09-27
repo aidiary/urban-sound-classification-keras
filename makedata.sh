643560488434b98375e276f92af4939577e9fb59
aidiary <f2forest@gmail.com>
2017-09-27 11:20:26 +0900

    Modify project structures

--- makedata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python makedata.py 128 2048 data/mel-128-2048
-python makedata.py 256 2048 data/mel-256-2048
-
-python makedata.py 128 1024 data/mel-128-1024
-python makedata.py 128 4096 data/mel-128-4096
